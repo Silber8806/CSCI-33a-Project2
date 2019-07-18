e6e8dd2468828a09e159429b58205d779fcc38d8
Chris <Silber8806@gmail.com>
2019-07-17 23:17:22 -0400

    better bottom scrolling

--- static/js/chat.js
+++ static/js/chat.js
@@ -157,7 +157,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 let current_message_board = document.getElementById("message_board");
                 console.log(document.documentElement.scrollHeight)
                 console.log(current_message_board.scrollHeight)
-                if (document.documentElement.scrollHeight === current_message_board.scrollHeight) {
+                if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
                     add_message(data.username, data.time, data.content);
                     message_board_scroll_bottom();
                 } else {
